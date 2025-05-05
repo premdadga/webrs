9ccff02a8c50426a0a58320fb76285e603cc1a31
Prem <premdadga@gmail.com>
2025-05-05 15:51:01 +0530

    minor

--- dist/yeq-49a760b1c19e1790.js
+++ dist/yeq-49a760b1c19e1790.js
@@ -235,7 +235,7 @@ function __wbg_adapter_20(arg0, arg1, arg2) {
 }
 
 function __wbg_adapter_23(arg0, arg1, arg2) {
-    wasm.closure168_externref_shim(arg0, arg1, arg2);
+    wasm.closure169_externref_shim(arg0, arg1, arg2);
 }
 
 async function __wbg_load(module, imports) {
@@ -558,12 +558,12 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper1895 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1948 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 112, __wbg_adapter_20);
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper2855 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 169, __wbg_adapter_23);
+    imports.wbg.__wbindgen_closure_wrapper2941 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 170, __wbg_adapter_23);
         return ret;
     };
     imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
--- src/comp.rs
+++ src/comp.rs
@@ -2,7 +2,18 @@ use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html{
+    let tasks = vec!["one", "two", "three"];
     html!{
-        <h1>{"hello world!"}</h1>
+        <>
+            <h1>{"hello world!"}</h1>
+            <u1>
+                {tasks.iter()
+                    .map(|task| html!(<li>{task}</li>))
+                    .collect::<Html>()}
+            </u1>
+        </>
     }
+
+    
+
 }
